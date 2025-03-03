2fbcea1f11fcbb9a1042e29d44ed3fa5f99b2b90
dhemeira <peterlakics2@gmail.com>
2025-03-03 20:22:00 +0100

    post request demo

--- FRONTEND/main.js
+++ FRONTEND/main.js
@@ -1,2 +1,28 @@
-let talajTipus = document.getElementById('#talaj-tipus');
-console.log(talajTipus.value);
+async function submit() {
+  const talajTipus = document.querySelector('#talaj-tipus').value;
+  const meres1 = document.querySelector('#meres-1').value;
+  const meres2 = document.querySelector('#meres-2').value;
+
+
+  fetch('http://localhost:5160/talajnedvesseg', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json', },
+    body: JSON.stringify({
+      matrixa: talajTipus,
+      matrixb: meres1,
+      matrixc: meres2,
+    })
+  })
+    .then(async resp => {
+      console.log('Response: ', resp)
+      if (resp.status === 200) {
+        const data = await resp.json()
+        displayResult(data)
+      }
+    })
+    .catch(error => console.log(error))
+}
+
+function displayResult(data) {
+  console.log(data)
+}
