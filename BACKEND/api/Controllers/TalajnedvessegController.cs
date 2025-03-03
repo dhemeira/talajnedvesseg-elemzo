e38ce9d984bf511901a364e727f83ab4c558438e
dhemeira <peterlakics2@gmail.com>
2025-03-03 21:21:47 +0100

    matrix szorzas

--- BACKEND/api/Controllers/TalajnedvessegController.cs
+++ BACKEND/api/Controllers/TalajnedvessegController.cs
@@ -10,6 +10,22 @@ public class TalajnedvessegController : ControllerBase
     [HttpPost(Name = "Talajnedvesség")]
     public List<List<double>> Post([FromBody] Talajnedvesseg talajnedvesseg)
     {
-        return talajnedvesseg.NormalizaltC;
+        return MatrixSzorzas(talajnedvesseg.MatrixB, talajnedvesseg.NormalizaltC);
+    }
+
+    private List<List<double>> MatrixSzorzas(List<List<double>> a, List<List<double>> b)
+    {
+        List<List<double>> result = new List<List<double>>(a.Count);
+
+        for (int i = 0; i < a.Count; i++)
+        {
+            result.Add(new List<double>(a[0].Count));
+            for (int j = 0; j < a[0].Count; j++)
+            {
+                result[i].Add(a[i][j] * b[i][j]);
+            }
+        }
+
+        return result;
     }
 }
